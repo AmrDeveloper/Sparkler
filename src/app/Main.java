9d3affe8953712ee53db20ea40c3512afbdffb58
AmrDeveloper <amr96@programmer.net>
2019-11-26 00:59:57 +0200

    add libraries for design

--- src/app/Main.java
+++ src/app/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/app/MainController.java
+++ src/app/MainController.java
@@ -1,4 +1,14 @@
 package app;
 
-public class MainController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
