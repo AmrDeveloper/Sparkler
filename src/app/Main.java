1ad389b854efec2b98e32742362e3e11b9462c2c
AmrDeveloper <amr96@programmer.net>
2019-11-26 00:58:05 +0200

    rename files

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,13 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main_view.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
