2fba2ffeb0dc6bd0c3cadcc709e718262c47e2c6
AmrDeveloper <amr96@programmer.net>
2019-11-26 01:23:05 +0200

    init all views in main_view

--- src/app/MainController.java
+++ src/app/MainController.java
@@ -1,11 +1,49 @@
 package app;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    @FXML private TextField searchTextField;
+    @FXML private ListView requestsListView;
+
+    @FXML private TextField httpRequestTextField;
+    @FXML private ComboBox<String> httpReqComboBox;
+    @FXML private Button sendRequestButton;
+
+    @FXML private Label statusLabel;
+    @FXML private Label timeLabel;
+
+    @FXML private TabPane requestTabPane;
+    @FXML private Tab requestParamsTab;
+    @FXML private Tab requestHeadersTab;
+    @FXML private Tab requestBodyTab;
+
+    @FXML private TabPane requestBodyTabPane;
+    @FXML private ComboBox<String> requestBodyRowComboBox;
+    @FXML private Button requestBodyRowCopyButton;
+    @FXML private Button requestBodyRowClearButton;
+
+    @FXML private TabPane responseTabPane;
+    @FXML private Tab responseBodyTab;
+    @FXML private Tab responseHeadersTab;
+    @FXML private ComboBox<String> responseBodyComboBox;
+    @FXML private Button responseBodyCopyButton;
+    @FXML private Button responseBodyClearButton;
+
+    @FXML private TextField socketUrlTextField;
+    @FXML private Button socketConnectButton;
+    @FXML private TextField socketEmitKey;
+    @FXML private TextField socketEmitValue;
+    @FXML private Button socketEmitButton;
+    @FXML private ListView socketEventListView;
+    @FXML private Button socketEventStartAll;
+    @FXML private Button socketEventStopAll;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
