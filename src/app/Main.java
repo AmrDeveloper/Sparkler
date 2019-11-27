7184e8080da90771a9b39bff040953f4fa2d9db7
AmrDeveloper <amr96@programmer.net>
2019-11-27 09:57:14 +0200

    set icon for Sparkler

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/main_view.fxml"));
         primaryStage.setTitle("Sparkler");
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/app/res/sparkler_icon.png")));
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
