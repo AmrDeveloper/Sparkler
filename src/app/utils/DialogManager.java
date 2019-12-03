e450e753ea09b815c7dc9d6970037df54cd71e05
AmrDeveloper <amr96@programmer.net>
2019-12-03 16:00:57 +0200

    Create DialogManager class

---
+++ src/app/utils/DialogManager.java
@@ -0,0 +1,52 @@
+package app.utils;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+public class DialogManager {
+
+    public interface OnDialogAction {
+        public void onAccept();
+        public void onRefuse();
+    }
+
+    public static void showInfoDialog(String title, String header, String content) {
+        Alert infoDialog = new Alert(Alert.AlertType.INFORMATION);
+        infoDialog.setTitle(title);
+        infoDialog.setHeaderText(header);
+        infoDialog.setContentText(content);
+        infoDialog.showAndWait();
+    }
+
+    public static void showWarningDialog(String title, String header, String content) {
+        Alert warnDialog = new Alert(Alert.AlertType.WARNING);
+        warnDialog.setTitle(title);
+        warnDialog.setHeaderText(header);
+        warnDialog.setContentText(content);
+        warnDialog.showAndWait();
+    }
+
+    public static void showErrorDialog(String title, String header, String content) {
+        Alert errorDialog = new Alert(Alert.AlertType.ERROR);
+        errorDialog.setTitle(title);
+        errorDialog.setHeaderText(header);
+        errorDialog.setContentText(content);
+        errorDialog.showAndWait();
+    }
+
+    public static void showConfirmDialog(String title, String header, String content, OnDialogAction action) {
+        Alert confirmDialog = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmDialog.setTitle(title);
+        confirmDialog.setHeaderText(header);
+        confirmDialog.setContentText(content);
+
+        Optional<ButtonType> result = confirmDialog.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            action.onAccept();
+        } else {
+            action.onRefuse();
+        }
+    }
+}
