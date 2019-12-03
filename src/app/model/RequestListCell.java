201ad919ca14022ed56aad0042007d1174ae557f
AmrDeveloper <amr96@programmer.net>
2019-12-03 13:05:24 +0200

    Set color for every request history

--- src/app/model/RequestListCell.java
+++ src/app/model/RequestListCell.java
@@ -4,6 +4,7 @@ import app.utils.SparklerListCell;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 public class RequestListCell extends SparklerListCell<Request> {
 
@@ -21,6 +22,30 @@ public class RequestListCell extends SparklerListCell<Request> {
     public void onViewUpdate() {
         requestUrlLabel.setText(getItem().getUrl());
         requestMethodLabel.setText(getItem().getMethod().name());
+
+        switch (getItem().getMethod()){
+            case GET:
+                //Green
+                requestMethodLabel.setTextFill(Color.web("#30d020", 0.8));
+                break;
+            case POST:
+                //Orange
+                requestMethodLabel.setTextFill(Color.web("#f9bd20", 0.8));
+                break;
+            case DELETE:
+                //Red
+                requestMethodLabel.setTextFill(Color.web("#e04d26", 0.8));
+                break;
+            case PUT:
+                //Blue
+                requestMethodLabel.setTextFill(Color.web("#4dcaef", 0.8));
+                break;
+            case PATCH:
+                //Purple
+                requestMethodLabel.setTextFill(Color.web("#c83ee7", 0.8));
+                break;
+        }
+
         setGraphic(requestViewLayout);
     }
 }
